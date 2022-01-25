db5c8af262267bcfba0fb1dfae27ac4e2f2d635e
Hélio Rocha <hjrocha@gmail.com>
2022-01-25 14:46:10 +0000

    test spotbugs

--- lib/src/main/java/com/codacy/test/Library.java
+++ lib/src/main/java/com/codacy/test/Library.java
@@ -3,8 +3,22 @@
  */
 package com.codacy.test;
 
+import javax.annotation.Nonnull;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NullCipher;
+
 public class Library {
     public boolean someLibraryMethod() {
         return true;
+    }
+
+    @Nonnull
+    public Object testObject(Object o, byte[] plainText) throws IllegalBlockSizeException, BadPaddingException{
+        Cipher doNothingCihper = new NullCipher();
+        byte[] cipherText = doNothingCihper.doFinal(plainText);
+
+        return o;
     }
 }
