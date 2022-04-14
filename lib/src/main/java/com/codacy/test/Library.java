9b8f076e878a5d1537ce707dab650710bfec00f9
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2022-04-14 15:58:21 +0100

    Update Library.java

--- lib/src/main/java/com/codacy/test/Library.java
+++ lib/src/main/java/com/codacy/test/Library.java
@@ -7,4 +7,8 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+    
+    public void getAccessKey() {
+        byte[] iv = "secret key in here".getBytes();
+    }
 }
